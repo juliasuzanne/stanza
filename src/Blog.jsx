ea1176d2b5f27f0900dbca6ea6afadcd7fec0054
Julia Grimes <julia.s.grimes@gmail.com>
2024-07-01 14:14:37 -0400

    Adjust blog page, fov

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,11 +1,8 @@
 export function Blog() {
   return (
     <div>
-      <p> Kate wanted a custom site built as an archive of drawings, dreams, and stories.</p>
-      <img src="/textures/img1.jpg"></img>
-      <p>
-        We worked together on mocking up the way it would look, adjusting to make sure it looked good on mobile as well.
-      </p>
+      <h1> This is a test page to link to </h1>
+      <p>Pages will store values of description, images, layout, fonts.</p>
     </div>
   );
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -3,7 +3,7 @@ import { Experience } from "./components/Experience";
 
 export function Home() {
   return (
-    <Canvas shadows camera={{ position: [0, 0, 5], fov: 80 }}>
+    <Canvas shadows camera={{ position: [0, 0, 5], fov: 10 }}>
       <color attach="background" args={["#ececec"]} />
       <Experience />
     </Canvas>
